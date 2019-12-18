d7bf1a771b4db179ac0e5ba9dfeb04c138d00b4c
SkripMen <48603232+SkripMen@users.noreply.github.com>
2019-12-18 14:51:25 +0500

    Update_3

--- Nikonov/application/models/model_mail.php
+++ Nikonov/application/models/model_mail.php
@@ -2,32 +2,32 @@
 
 class Model_Mail extends Model
 {
-    public function get_result($array)
+    public function get_result($mail = null, $name = null, $title = null, $text = null)
     {
         $data = 'Всё не норм';
-        if (!filter_var($array['mail'], FILTER_VALIDATE_EMAIL)) {
+        if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
             $data = 'Ошибка введения почты';
             return $data;
         }
 
-        if (empty($array['name'])){
+        if (empty($name)){
             $data = 'Вы не ввели имя';
             return $data;
         }
 
-        if (empty($array['title'])) {
+        if (empty($title)) {
             $data = 'Вы не задали тему';
             return $data;
         }
 
-        if (empty($array['text'])) {
+        if (empty($text)) {
             $data = 'Вы не написали сообщение';
             return $data;
         }
 
-        if (mail($array['mail'], trim($array['title']), trim($array['text']))) {
-            $data = 'Вам было отправлено сообщение от пользователя: ' . $array['name'] . '. На почту: (' . $array['mail'] . ') Тема сообщения: ' . $array['title'] .
-                '. Текст сообщения: ' . $array['text'];
+        if (mail($mail, trim($title), trim($text))) {
+            $data = 'Вам было отправлено сообщение от пользователя: ' . $name . '. На почту: (' . $mail . ') Тема сообщения: ' . $array['title'] .
+                '. Текст сообщения: ' . $text;
         }
         return $data;
     }
