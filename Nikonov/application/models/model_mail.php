75c97162a043ac5827b124eabe6769da80a483c5
SkripMen <48603232+SkripMen@users.noreply.github.com>
2020-03-06 20:13:19 +0400

    Add files via upload

--- Nikonov/application/models/model_mail.php
+++ Nikonov/application/models/model_mail.php
@@ -4,31 +4,35 @@ class Model_Mail extends Model
 {
     public function get_result($mail = null, $name = null, $title = null, $text = null)
     {
-        $result = "";
-        $good = true;
+        // $result = "";
+        // $error = array();
         
         if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-            $result .= 'Ошибка введения почты!</br>' . PHP_EOL;
-            $good = false;
+            $errors[] .= 'Ошибка введения почты!</br>' . PHP_EOL;
         }
 
         if (empty(trim($name))) {
-            $result .= 'Вы не ввели имя!</br>' . PHP_EOL;
-            $good = false;
+            $errors[] .= 'Вы не ввели имя!</br>' . PHP_EOL;
+
         }
 
         if (empty(trim($title))) {
-            $result .= 'Вы не задали тему!</br>' . PHP_EOL;
-            $good = false;
+            $errors[] .= 'Вы не задали тему!</br>' . PHP_EOL;
+
         }
 
         if (empty(trim($text))) {
-            $result .= 'Вы не написали сообщение!</br>' . PHP_EOL;
-            $good = false;
+            $errors[] .= 'Вы не написали сообщение!</br>' . PHP_EOL;
+
         }
 
-        if ($good && mail($mail, trim($title), trim($text))) {
+        if (empty($errors) && mail($mail, trim($title), trim($text))) {
             $result = 'Вам было отправлено сообщение от пользователя: ' . $name . '.</br>На почту: (' . $mail . ').</br> Тема сообщения: ' . $title . '. </br>Текст сообщения: ' . $text;
+        }
+        else{
+            foreach ($errors as $value){
+                echo $value;
+            }
         }
         
         
