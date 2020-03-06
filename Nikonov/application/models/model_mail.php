f29a055ee7c079997e806761d3d9f88aa5104c0b
SkripMen <48603232+SkripMen@users.noreply.github.com>
2020-03-06 18:52:02 +0400

    Add files via upload

--- Nikonov/application/models/model_mail.php
+++ Nikonov/application/models/model_mail.php
@@ -5,30 +5,32 @@ class Model_Mail extends Model
     public function get_result($mail = null, $name = null, $title = null, $text = null)
     {
         $result = "";
-        if (empty(trim($text) && empty(trim($title))) && empty(trim($name)) && !filter_var($mail, FILTER_VALIDATE_EMAIL)){
-            $result .= 'Вы неправильно заполнили форму!</br>-Ошибка введения почты!</br>-Вы не ввели имя!</br>-Вы не задали тему!</br>-Вы не написали сообщение!</br>' . PHP_EOL;
+        $good = true;
+        
+        if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+            $result .= 'Ошибка введения почты!</br>' . PHP_EOL;
+            $good = false;
         }
-        else{
-            if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-                $result .= 'Ошибка введения почты!</br>' . PHP_EOL;
-            }
 
-            if (empty(trim($name))) {
-                $result .= 'Вы не ввели имя!</br>' . PHP_EOL;
-            }
-
-            if (empty(trim($title))) {
-                $result .= 'Вы не задали тему!</br>' . PHP_EOL;
-            }
-
-            if (empty(trim($text))) {
-                $result .= 'Вы не написали сообщение!</br>' . PHP_EOL;
-            }
-
-            if (mail($mail, trim($title), trim($text))) {
-                    $result = 'Вам было отправлено сообщение от пользователя: ' . $name . '.</br>На почту: (' . $mail . ').</br> Тема сообщения: ' . $title . '. </br>Текст сообщения: ' . $text;
-                }
+        if (empty(trim($name))) {
+            $result .= 'Вы не ввели имя!</br>' . PHP_EOL;
+            $good = false;
         }
+
+        if (empty(trim($title))) {
+            $result .= 'Вы не задали тему!</br>' . PHP_EOL;
+            $good = false;
+        }
+
+        if (empty(trim($text))) {
+            $result .= 'Вы не написали сообщение!</br>' . PHP_EOL;
+            $good = false;
+        }
+
+        if ($good && mail($mail, trim($title), trim($text))) {
+            $result = 'Вам было отправлено сообщение от пользователя: ' . $name . '.</br>На почту: (' . $mail . ').</br> Тема сообщения: ' . $title . '. </br>Текст сообщения: ' . $text;
+        }
+        
         
         
         return $result;
